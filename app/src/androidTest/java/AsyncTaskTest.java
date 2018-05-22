ac8181838fa75da566f7499d97e893cc9d735ea9
lpelczar <lukepelczar@gmail.com>
2018-05-22 18:12:23 +0200

    Fix async test to use count down latch

--- app/src/androidTest/java/AsyncTaskTest.java
+++ app/src/androidTest/java/AsyncTaskTest.java
@@ -3,28 +3,26 @@ import com.udacity.gradle.builditbigger.OnJokeRetrieve;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static junit.framework.Assert.assertNotNull;
 
 public class AsyncTaskTest {
 
     @Test
     public void testAsyncJokeRetrieve() throws InterruptedException, ExecutionException, TimeoutException {
-
-        String joke;
+        final CountDownLatch signal = new CountDownLatch(1);
         EndpointAsyncTask jokeTask = new EndpointAsyncTask(new OnJokeRetrieve() {
 
             @Override
             public void onJokeRetrieve(String joke) {
+                assertNotNull(joke);
+                signal.countDown();
             }
         });
         jokeTask.execute();
-        joke = jokeTask.get(30, TimeUnit.SECONDS);
-        Thread.sleep(SECONDS.toMillis(2));
-        assertNotNull(joke);
+        signal.await();
     }
 }
